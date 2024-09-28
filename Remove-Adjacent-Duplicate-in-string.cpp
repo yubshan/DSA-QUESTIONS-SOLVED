ce7a4813471d34d376db3e754d6a3069b1bbc29b
yubshan <shresthayubshan09@gmail.com>
2024-09-29 00:03:30 +0545

    new question solved

--- Remove-Adjacent-Duplicate-in-string.cpp
+++ Remove-Adjacent-Duplicate-in-string.cpp
@@ -2,29 +2,21 @@
 #include<string>
 using namespace std;
 string removeDuplicates(string s) {
-        for (int i =0 ; i < s.length() ; i++){
-            if (s[i] == s[i+1]){
-                cout<<i<< endl<<"entered" << endl << s << endl;
-                s.erase(i,i+1);
-                cout<<"erased" << endl << s << endl;
+    int i = 0 ;
+    while ( i < s.length()){
+        if (i<s.length()-1 && s[i] == s[i+1]){
+                s.erase(i,2);
                 i--;
-                cout<<i<<endl;
             }
-            else if(s[i] == s[i-1]){
-                cout <<endl;
-                cout<<i<< endl << "entered non" << endl << s << endl;
-                s.erase(i-1, i+1);
-                cout<<"erased2" << endl << s << endl;
+            else{
+            i++;
             }
-
-            
-        }
+    }
         return s;
     }
 int main(){
     string s;
     cin >>s;
-    cout << s.erase(0,1);
-    // cout << "Your final answer is : " << removeDuplicates(s)<< endl; 
+    cout << "Your final answer is : " << removeDuplicates(s)<< endl; 
     return 0;
 }
